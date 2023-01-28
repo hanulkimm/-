416de75e6126e89cc6ef853567d0a93c378de4e0
hanulkimm <122726684+hanulkimm@users.noreply.github.com>
2023-01-28 22:55:32 +0900

    [Bronze II] Title: OX퀴즈, Time: 44 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.py
@@ -0,0 +1,15 @@
+n= int(input())
+
+for _ in range(n):
+    case = input()
+
+    score = 0
+    total = 0
+
+    for i in case:
+        if i == 'O':
+            score +=1
+            total += score
+        else:
+            score = 0
+    print(total)
