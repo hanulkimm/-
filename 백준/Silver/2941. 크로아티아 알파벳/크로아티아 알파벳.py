2e232ba34f8d5f4de501a13ebc7aa562cd9ec0a2
hanulkimm <122726684+hanulkimm@users.noreply.github.com>
2023-01-30 16:41:39 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
@@ -1,24 +1,31 @@
+# 내 코드
 lst = ['c=','c-','dz=','d-','lj','nj','s=','z=']
 croatia = input()
 
 
-def a(croatia):
-    word = []
+# def a(croatia):
+#     word = []
 
-    def new(croatia):
-        while len(croatia) >= 1:
-            if croatia[0:2] in lst :
-                word.append(1)
-                new(croatia.replace(croatia[0:2],'',1))
-            elif croatia[0:3] in lst:
-                word.append(1)
-                new(croatia.replace(croatia[0:3],'',1))
-            else:
-                word.append(1)
-                new(croatia.replace(croatia[0],'',1))
-            break
+#     def new(croatia):
+#         while len(croatia) >= 1:
+#             if croatia[0:2] in lst :
+#                 word.append(1)
+#                 new(croatia.replace(croatia[0:2],'',1))
+#             elif croatia[0:3] in lst:
+#                 word.append(1)
+#                 new(croatia.replace(croatia[0:3],'',1))
+#             else:
+#                 word.append(1)
+#                 new(croatia.replace(croatia[0],'',1))
+#             break
             
-    new(croatia)
-    return sum(word)
+#     new(croatia)
+#     return sum(word)
 
-print(a(croatia))
+#print(a(croatia))
+
+# 더 쉬운 접근
+for i in lst:
+    croatia = croatia.replace(i,'*')
+print(len(croatia))
+
