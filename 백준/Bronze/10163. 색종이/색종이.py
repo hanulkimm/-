31b95baafc6d99a51dd871045cd7aacaa148af94
hanulkimm <122726684+hanulkimm@users.noreply.github.com>
2023-02-26 21:11:51 +0900

    [Bronze I] Title: 색종이, Time: 492 ms, Memory: 121632 KB -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.py
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -1,14 +1,15 @@
+import sys
 n = int(input())
-ans  =0
 arr = [[0]*1001 for _ in range(1001)]
 for num in range(1,n+1):
-    x, y, w, h = map(int, input().split())
+    x, y, w, h = map(int, sys.stdin.readline().split())
     for i in range(x,x+w):
         for j in range(y, y+h):
             arr[i][j]=num
-
-for i in range(1,n+1):
-    cnt = 0
-    for lst in arr:
-        cnt += lst.count(i)
-    print(cnt)
+cnt = [0] * (n+1)
+for i in range(1001):
+    for j in range(1001):
+        if arr[i][j]:
+            cnt[arr[i][j]] += 1
+for i in range(1, n+1):
+    print(cnt[i])
