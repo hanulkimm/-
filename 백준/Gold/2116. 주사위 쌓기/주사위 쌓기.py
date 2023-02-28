a9157edab542f894429add5fbca697b3b02ce285
hanulkimm <122726684+hanulkimm@users.noreply.github.com>
2023-02-28 17:47:23 +0900

    [Gold V] Title: 주사위 쌓기, Time: 516 ms, Memory: 32276 KB -BaekjoonHub

---
+++ 백준/Gold/2116. 주사위 쌓기/주사위 쌓기.py
@@ -0,0 +1,39 @@
+dct = {0: 5, 1: 3, 2: 4, 3: 1, 4: 2, 5: 0}
+
+def find_mx(idx1, idx2, i):
+    full = [0, 1, 2, 3, 4, 5]
+    full.remove(idx1)
+    full.remove(idx2)
+    mx = 0
+    for j in full:
+        if mx < arr[i][j]:
+            mx = arr[i][j]
+    return mx
+
+def repeat(top):
+    global ans
+    idx = -1
+    for i in range(1, n): # 주사위 2번부터
+         for j in range(6): # 주사위 숫자들
+             if arr[i][j]==top:
+                 idx = j
+         top = arr[i][dct[idx]]
+         ans += find_mx(idx, dct[idx],i)
+
+
+
+n = int(input())
+arr = []
+for i in range(n):
+    lst = list(map(int, input().split()))
+    arr.append(lst)
+
+real = 0
+for i in range(6):
+    ans = 0
+    top = arr[0][i]
+    ans += find_mx(i, dct[i], 0)
+    repeat(top)
+    if real < ans:
+        real = ans
+print(real)
