f51b744b865d938eac74e7a5b501a483837a4529
hanulkimm <122726684+hanulkimm@users.noreply.github.com>
2023-03-01 21:09:09 +0900

    [Silver I] Title: 직사각형, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/2527. 직사각형/직사각형.py
@@ -0,0 +1,14 @@
+for _ in range(4):
+    x1,y1,x2,y2,p1,q1,p2,q2 = map(int, input().split())
+    # [1] 안 만날 때
+    if (x2 < p1) or (p2 < x1) or (y2 < q1) or (q2 < y1):
+        ans = 'd'
+    # [3] 선분일 때
+    elif (y2 == q1 and x1 < p2 and p1 < x2) or (x2 == p1 and q1 < y2 and y1 < q2) or (y1 == q2 and p1 < x2 and x1 < p2) or (x1 == p2 and q1 < y2 and y1 < q2):
+        ans = 'b'
+    # [2] 점 일때
+    elif (x2, y2)==(p1,q1) or (x2, y1)==(p1,q2) or (x1,y1)==(p2,q2) or (x1,y2)==(p2,q1):
+        ans = 'c'
+    else:
+        ans = 'a'
+    print(ans)
