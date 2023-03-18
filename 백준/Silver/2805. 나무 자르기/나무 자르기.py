e2fd27a89c52c1d3f83df5e3d09957e4e267560e
hanulkimm <122726684+hanulkimm@users.noreply.github.com>
2023-03-18 21:49:29 +0900

    [Silver II] Title: 나무 자르기, Time: 4540 ms, Memory: 150704 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -3,6 +3,7 @@ lst = list(map(int, input().split()))
 h = max(lst)
 s = 0
 e = h
+ans = 0
 while s <= e:
     m = (s+e)//2
     cnt = 0
@@ -10,8 +11,9 @@ while s <= e:
         if ch > m:
             cnt += ch - m
     if cnt >= M:
+        ans = m
         s = m+1
     elif cnt < M:
         e = m-1
 
-print(e)
+print(ans)
