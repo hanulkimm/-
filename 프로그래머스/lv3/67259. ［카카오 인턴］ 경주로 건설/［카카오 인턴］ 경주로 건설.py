44d5b0c120fc8d53c873198650a15fe56081c55d
hanulkimm <122726684+hanulkimm@users.noreply.github.com>
2023-03-21 20:30:34 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 0.07 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
+++ 프로그래머스/lv3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
@@ -1,26 +1,24 @@
 from collections import deque
+
 def solution(board):
     n = len(board)
-    directions = ((-1,0),(1,0),(0,-1),(0,1)) # 상하좌우
+    answer = 1e9
+    v = [[0]*n for _ in range(n)]
+    directions = [(-1, 0, 0), (0, 1, 1), (1, 0, 2), (0, -1, 3)] # 상하좌우 좌표, 방향 index
+    q = deque([(0,0,0,-1)]) # 시작좌표, cost, 방향
 
-    def bfs(x,y,cost,d):
-        v = [[0]*n for _ in range(n)] # 방문 표시 대신 최소비용 표시
-        q = deque()
-        q.append((x,y,cost,d))
-
-        while q:
-            x, y, cost, dr = q.popleft()
-            for i in range(4):
-                ni, nj = x + directions[i][0], y + directions[i][1]
-
-                if 0<=ni<n and 0<=nj<n and board[ni][nj]==0: # 범위내, 벽 아님
-                    if dr==i: # 같은 방향
-                        newcost = cost + 100
-                    else: # 같은 방향 아님, 코너 생김
-                        newcost = cost + 600
-
-                    if v[ni][nj]==0 or  v[ni][nj]>newcost: # 첫방문이거나 이미 방문한 경우 보다 값이 작은 경우
-                        q.append((ni,nj,newcost,i))
-                        v[ni][nj] = newcost # 값 갱신
-        return v[n-1][n-1] # 도착점
-    return min(bfs(0,0,0,1), bfs(0,0,0,3)) # 아래로 출발, 오른쪽으로 출발 (2가지 경우)
+    while q:
+        i,j,cost,dir_idx = q.popleft()
+        if (i,j)==(n-1,n-1) and answer>cost:
+            answer = cost
+        for direction in directions:
+            ni,nj = i+direction[0], j+direction[1]
+            if 0 <= ni < n and 0 <= nj < n and board[ni][nj] == 0:
+                if dir_idx==-1 or dir_idx==direction[2]:
+                    add_cost = 100
+                else:
+                    add_cost = 600
+                if v[ni][nj]==0 or v[ni][nj]+500>cost+add_cost:
+                    q.append((ni,nj,cost+add_cost,direction[2]))
+                    v[ni][nj]=cost+add_cost
+    return answer
