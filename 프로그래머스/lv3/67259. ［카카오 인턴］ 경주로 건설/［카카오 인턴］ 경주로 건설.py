3a1b5c4ac47baffc6edcc015aff5dbe3914a811c
hanulkimm <122726684+hanulkimm@users.noreply.github.com>
2023-03-21 19:48:42 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 0.08 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
+++ 프로그래머스/lv3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
@@ -1,33 +1,26 @@
 from collections import deque
 def solution(board):
-    N = len(board)
-    directions = [(-1,0), (0,-1), (1,0), (0,1)] # 상좌하우
-    
-    def bfs(x,y,cost,d): 
-        graph = [[0]*N for _ in range(N)]
-        for a in range(N):
-            for b in range(N):
-                if board[a][b] == 1 : graph[a][b] = -1  # 벽을 -1로 설정
+    n = len(board)
+    directions = ((-1,0),(1,0),(0,-1),(0,1)) # 상하좌우
+
+    def bfs(x,y,cost,d):
+        v = [[0]*n for _ in range(n)] # 방문 표시 대신 최소비용 표시
         q = deque()
         q.append((x,y,cost,d))
+
         while q:
-            x,y,cost,idx = q.popleft()
-            for i in range(len(directions)):
-                nx = x + directions[i][0]
-                ny = y + directions[i][1]
-                
-                if nx < 0 or nx >= N or ny < 0 or ny >= N : continue
-                if graph[nx][ny] == -1 : continue
+            x, y, cost, dr = q.popleft()
+            for i in range(4):
+                ni, nj = x + directions[i][0], y + directions[i][1]
 
-                if idx == i : newcost = cost + 100
-                else : newcost = cost + 600
-                
-                if graph[nx][ny] == 0 or ((graph[nx][ny] != 0) and graph[nx][ny] > newcost):
-                    q.append((nx,ny,newcost,i))
-                    graph[nx][ny] = newcost
-                    
-                else : continue
+                if 0<=ni<n and 0<=nj<n and board[ni][nj]==0: # 범위내, 벽 아님
+                    if dr==i: # 같은 방향
+                        newcost = cost + 100
+                    else: # 같은 방향 아님, 코너 생김
+                        newcost = cost + 600
 
-        return graph[N-1][N-1]
-
-    return min(bfs(0,0,0,2),bfs(0,0,0,3))
+                    if v[ni][nj]==0 or  v[ni][nj]>newcost: # 첫방문이거나 이미 방문한 경우 보다 값이 작은 경우
+                        q.append((ni,nj,newcost,i))
+                        v[ni][nj] = newcost # 값 갱신
+        return v[n-1][n-1] # 도착점
+    return min(bfs(0,0,0,1), bfs(0,0,0,3)) # 아래로 출발, 오른쪽으로 출발 (2가지 경우)
