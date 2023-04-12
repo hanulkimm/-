22825e7efe343f87dd52369110f2580033da7d4b
hanulkimm <122726684+hanulkimm@users.noreply.github.com>
2023-04-12 21:59:49 +0900

    [Gold V] Title: 집합의 표현, Time: 304 ms, Memory: 76720 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.py
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.py
@@ -1,20 +1,30 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(100000)
+
 def find_set(x):
-    while x!=rep[x]:
-        x = rep[x]
-    return x
+    if x == parent[x]:
+        return x
+    parent[x] = find_set(parent[x])
+    return parent[x]
 
 def union(x,y):
-    rep[find_set(y)]=find_set(x)
+    x, y = find_set(x), find_set(y)
+
+    if x < y: # 값이 더 작은 쪽이 부모가 되도록
+        parent[y] = x
+    else:
+        parent[x] = y
+
 
 n, m = map(int, input().split())
-rep = [i for i in range(n+1)]
-
+parent = [i for i in range(n+1)]
 for _ in range(m):
     i, a, b = map(int, input().split())
-    if i:
-        if find_set(a) == find_set(b):
-            print('YES')
-        else:
-            print('NO')
+    if i==0:
+        union(a,b)
     else:
-        union(a, b)
+        if find_set(a)==find_set(b):
+            print('yes')
+        else:
+            print('no')
