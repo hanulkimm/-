a4d2384e19ca2821773a8f3484c3b352272bfa87
hanulkimm <122726684+hanulkimm@users.noreply.github.com>
2023-04-16 23:08:12 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 312 ms, Memory: 18120 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i < n+1; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+}
