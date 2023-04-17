e278d34078e32005457a95f40efc018be66f9e80
hanulkimm <122726684+hanulkimm@users.noreply.github.com>
2023-04-17 20:01:29 +0900

    [Bronze II] Title: 나머지, Time: 204 ms, Memory: 17672 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -1,23 +1,20 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashSet;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         Scanner sc= new Scanner(System.in);
-        int[] leftover = new int[42];
+        HashSet<Integer> h = new HashSet<Integer>();
         for (int i = 0; i < 10; i++) {
-            int num = sc.nextInt();
-            leftover[num%42]=1;
+            int num = sc.nextInt() % 42;
+            h.add(num);
         }
-        int ans = 0;
-        for (int i = 0; i < 42; i++) {
-            if (leftover[i]==1) {
-                ans ++;
-            }
+        sc.close();
+        System.out.println(h.size());
         }
-        System.out.println(ans);
+
     }
-}
