055da710ade5c5f565bbaeea050d0c5e5b77421e
hanulkimm <122726684+hanulkimm@users.noreply.github.com>
2023-04-18 17:36:29 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 312 ms, Memory: 18308 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[26];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]=-1;
+        }
+        String s = sc.next();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (arr[ch-'a']==-1) {
+                arr[ch-'a'] = i;
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        };
+    }
+}
