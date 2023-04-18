610ff551e075aaf84fe6cc9edd36d0ae35a435e9
hanulkimm <122726684+hanulkimm@users.noreply.github.com>
2023-04-18 22:11:01 +0900

    [Bronze II] Title: 단어의 개수, Time: 264 ms, Memory: 20000 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.java
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -6,14 +6,8 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s = br.readLine();
-        String[] sent = s.split(" ");
-        int ans = 0;
-        for (String num : sent) {
-            if (num.length()>0) {
-                ans +=1;
-            }
-        }
-        System.out.println(ans);
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        System.out.println(st.countTokens());
         }
     }
