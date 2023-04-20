352be847a94a778eea1880d8a01a3478b49c69f6
hanulkimm <122726684+hanulkimm@users.noreply.github.com>
2023-04-20 17:32:23 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 248 ms, Memory: 25328 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            String s = sc.next();
+            Boolean flag = false;
+
+            for (int j = 0; j < s.length(); j++) {
+                for (int k = s.indexOf(s.charAt(j)); k < s.lastIndexOf(s.charAt(j))+1; k++) {
+                    if (!Character.toString(s.charAt(k)).equals(Character.toString(s.charAt(j)))) {
+                        flag =true;
+                    }
+                    if (flag) {break; }
+                }
+            }
+            if (!flag) {
+                ans += 1;
+            }
+        }
+        System.out.println(ans);
+    }
+}
+
