abc993f70b8cc53770a4273edd687cc47bb79568
hanulkimm <122726684+hanulkimm@users.noreply.github.com>
2023-04-20 16:47:52 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 200 ms, Memory: 17564 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -6,41 +6,15 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
-        StringBuilder sb = new StringBuilder(sc.next());
+        String st = sc.next();
         String[] croat = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
-        int ans = 0;
-        int i = 0;
-        while (i<sb.length()) {
-            Boolean flag = true;
-            if (i<sb.length()-2) {
-                String s3 = sb.substring(i, i + 3);
-                for (String ch : croat) {
-                    if (s3.equals(ch)) {
-                        ans += 1;
-                        flag = false;
-                        i += 3;
-                        break;
-                    }
-                }
-            }
-            if (flag &&  i<sb.length()-1) {
-                String s2 = sb.substring(i, i + 2);
-                for (String ch : croat) {
-                    if (s2.equals(ch)) {
-                        ans += 1;
-                        flag = false;
-                        i += 2;
-                        break;
-                    }
-                }
-            }
-
-            if (flag) {
-                ans += 1;
-                i+=1;
+        for (int i = 0; i < croat.length; i++) {
+            if (st.contains(croat[i])) {
+                st = st.replace(croat[i], ".");
             }
         }
-        System.out.println(ans);
+        System.out.println(st.length());
+
     }
 }
 
