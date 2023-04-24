b53ee277fbc2dfd3579a5a1c1ff80f4ddcc9e941
hanulkimm <122726684+hanulkimm@users.noreply.github.com>
2023-04-24 16:03:59 +0900

    [Silver I] Title: 단지번호붙이기, Time: 64 ms, Memory: 34160 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,31 +1,26 @@
-
 from collections import deque
-
-def bfs(i,j):
-    tmp = 1
+def count(i,j):
     q = deque()
     q.append((i,j))
     arr[i][j]=0
+    cnt = 1
     while q:
-        si,sj = q.popleft()
+        ci,cj = q.popleft()
         for di,dj in ((-1,0),(1,0),(0,-1),(0,1)):
-            ni,nj = si+di, sj+dj
+            ni,nj = ci+di, cj+dj
             if 0<=ni<n and 0<=nj<n and arr[ni][nj]==1:
                 arr[ni][nj]=0
-                tmp += 1
+                cnt+=1
                 q.append((ni,nj))
-    cnt.append(tmp)
-
+    ans.append(cnt)
 n = int(input())
 arr = [list(map(int, input())) for _ in range(n)]
-ans = 0
-cnt = []
+ans = []
 for i in range(n):
     for j in range(n):
-        if arr[i][j]==1:
-            ans += 1
-            bfs(i,j)
-print(ans)
-cnt.sort()
-for num in cnt:
+        if arr[i][j]:
+            count(i,j)
+ans.sort()
+print(len(ans))
+for num in ans:
     print(num)
