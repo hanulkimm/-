3d3648736de8928663b08161e0e779b25666816e
hanulkimm <122726684+hanulkimm@users.noreply.github.com>
2023-04-24 21:56:13 +0900

    [Gold IV] Title: 월드컵, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/6987. 월드컵/월드컵.py
@@ -0,0 +1,28 @@
+from itertools import combinations
+games = list(combinations(range(6), 2))
+
+def dfs(depth):
+    global ans
+    if depth==15:
+        ans = 1
+        for result in results:
+            if sum(result)!=0:
+                ans =0
+                break
+        return
+    g1, g2 = games[depth]
+    for x, y in ((0, 2), (2, 0), (1, 1)):
+        if results[g1][x]>0 and results[g2][y]>0:
+            results[g1][x] -= 1
+            results[g2][y] -= 1
+            dfs(depth+1)
+            results[g1][x] += 1
+            results[g2][y] += 1
+
+
+for _ in range(4):
+    tmp = list(map(int, input().split()))
+    results = [tmp[i*3:i*3+3] for i in range(0,6)]
+    ans = 0
+    dfs(0)
+    print(ans, end=' ')
