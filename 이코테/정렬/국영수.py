f5ad20767fcdcaba4e4355aaa4fccfd2b84d17a4
hanulkimm <hanulkim2000@naver.com>
2023-05-10 22:36:08 +0900

    add 정렬

---
+++ 이코테/정렬/국영수.py
@@ -0,0 +1,10 @@
+n = int(input())
+arr = [list(input().split()) for _ in range(n)]
+# arr.sort(key=lambda x:x[0]) # 이름 오름차순
+# arr.sort(key=lambda x:int(x[3]), reverse=True) # 수학 내림차순
+# arr.sort(key=lambda x:int(x[2])) # 영어 점수 오름차순
+# arr.sort(key=lambda x:int(x[1]), reverse=True) # 국어 내림차순
+
+arr.sort(key=lambda x:(-int(x[3]),int(x[2]), -int(x[1]),x[0]))
+for lst in arr:
+    print(lst[0])
