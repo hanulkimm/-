c6b64f47ed0b375015e6621f193d1413794a804a
hanulkimm <122726684+hanulkimm@users.noreply.github.com>
2023-06-03 17:02:32 +0900

    [Silver III] Title: 퇴사, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -1,10 +1,8 @@
 n = int(input())
-lst = [list(map(int, input().split())) for _ in range(n)]
-
-dp = [0] * (n+1) # 0~n
+arr = [list(map(int, input().split())) for _ in range(n)]
+dp = [0] * (n+1)
 for i in range(n):
-    for j in range(i+lst[i][0], n+1): # 상담 끝나는 날부터 끝까지
-        if dp[j] < dp[i] + lst[i][1]: # 상담 추가 될 때마다 중복되지 않게 할 수 있는 이전 상담 금액 추가
-            dp[j] = dp[i] + lst[i][1]
-
+    for j in range(i+arr[i][0], n+1):
+        if dp[j] < dp[i] + arr[i][1]:
+            dp[j] = dp[i] + arr[i][1]
 print(dp[-1])
