d419adfd8ba6f1a55833a3692b78e8ba8911cc38
hanulkimm <122726684+hanulkimm@users.noreply.github.com>
2023-08-31 20:01:30 +0900

    [Bronze III] Title: ACM 호텔, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
@@ -1,12 +1,16 @@
 t = int(input())
 for _ in range(t):
-    h,w,n= (map(int, input().split()))
-
-    floor = n % h
-    num = n // h + 1
-    if n % h == 0:
-        num = n//h
+    h, w, n = map(int, input().split())
+    # n%h==0 일때 아닐때
+    if n%h==0:
         floor = h
-
-    print(num + floor * 100)
-          
+        room = n//h
+    else:
+        floor = n%h
+        room = n//h+1
+    
+    if room<10:
+        ans = str(floor) + str('0') + str(room)
+    else:
+        ans = str(floor) + str(room)
+    print(ans)
