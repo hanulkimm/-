488311bfcd7b989167a449333c88e23f16672750
hanulkimm <122726684+hanulkimm@users.noreply.github.com>
2023-09-02 18:23:48 +0900

    [Bronze II] Title: 블랙잭, Time: 104 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,11 +1,10 @@
+
+from itertools import combinations
 n,m = map(int, input().split())
 lst = list(map(int, input().split()))
-ans = 0
-for i in range(n):
-    for j in range(n):
-        for k in range(n):
-            if i!=j and j!=k and k!=i:
-                cnt = lst[i]+lst[j]+lst[k]
-                if m-cnt>=0 and (m-ans >= m-cnt):
-                    ans = cnt
-print(ans)
+comb = combinations(lst,3)
+mx = 0
+for i in comb:
+    if sum(i)<=m and mx<sum(i):
+        mx=sum(i)
+print(mx)
