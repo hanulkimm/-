af96f8b8c65b97f40130472ee2a87628f4dc8acf
hanulkimm <122726684+hanulkimm@users.noreply.github.com>
2023-09-03 23:42:47 +0900

    [Silver IV] Title: 설탕 배달, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,15 +1,9 @@
 n = int(input())
-
-rst = []          
-for i in range(1001):
-    for j in range(1667):
-
-        if 5*i + 3*j == n:
-            rst.append(i+j)
-            
-        else:
-            continue
-if rst == []:
-    print(-1)
-else:
-    print(min(rst))
+ans = n
+for i in range(n//5,-1,-1):
+    if (n-i*5)%3==0:
+       if ans > i + (n-i*5)//3:
+           ans = i + (n-i*5)//3
+if ans ==n:
+    ans = -1
+print(ans)
