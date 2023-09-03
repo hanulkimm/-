da936e00d6dc7d9fcedbf4802daf78683135beac
hanulkimm <122726684+hanulkimm@users.noreply.github.com>
2023-09-03 15:19:19 +0900

    [Silver V] Title: 덩치, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.py
+++ 백준/Silver/7568. 덩치/덩치.py
@@ -1,24 +1,13 @@
 n = int(input())
-arr = [list(map(int, input().split())) for _ in range(n)]
-
+lst = []
+for _ in range(n):
+    lst.append(tuple(map(int, input().split())))
+ans = []
 for i in range(n):
-    cnt = 1
-    w, h = arr[i][0], arr[i][1]
-    if i == 0:
-        for j in range(i+1, n):
-            if w < arr[j][0] and h < arr[j][1]:
-                cnt += 1
-        print(cnt, end=' ')
-    elif i == n-1:
-        for j in range(n):
-            if w < arr[j][0] and h < arr[j][1]:
-                cnt += 1
-        print(cnt, end=' ')
-    else:
-        for j in range(i):
-            if w < arr[j][0] and h < arr[j][1]:
-                cnt += 1
-        for k in range(i+1, n):
-            if w < arr[k][0] and h < arr[k][1]:
-                cnt += 1
-        print(cnt, end = ' ')
+    tmp = 1
+    for j in range(n):
+        if lst[i][0]<lst[j][0] and lst[i][1]<lst[j][1]:
+           tmp += 1
+    ans.append(tmp)
+for i in ans:
+    print(i, end=' ')
