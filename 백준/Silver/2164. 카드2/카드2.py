96572fc77382a139154a9b7a30b7f35cdf1fc947
hanulkimm <122726684+hanulkimm@users.noreply.github.com>
2023-09-03 23:37:51 +0900

    [Silver IV] Title: 카드2, Time: 228 ms, Memory: 54984 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -1,9 +1,8 @@
 from collections import deque
 n = int(input())
-lst = [i for i in range(1,n+1)]
-deq = deque(lst)
+q = deque([i for i in range(1, n+1)])
+while len(q)>1:
+    q.popleft()
+    q.append(q.popleft())
 
-while len(deq)>1:
-    deq.popleft()
-    deq.append(deq.popleft())
-print(*deq)
+print(*q)
